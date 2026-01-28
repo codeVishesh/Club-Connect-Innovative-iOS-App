50a14554adc3fd8d9ebcc0d41a6bde211f0ccf62
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2026-01-28 23:53:43 +0530

    Add files via upload

---
+++ Club_ConnectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Club_ConnectTests.swift
+//  Club ConnectTests
+//
+//  Created by Vishesh Gupta on 21/11/24.
+//
+
+import Testing
+@testable import Club_Connect
+
+struct Club_ConnectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
