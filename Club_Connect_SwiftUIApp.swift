744d70dcd93a3c4ab557fbdc9ebbad5870c4749f
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2026-02-01 21:47:44 +0530

    Add files via upload

---
+++ Club_Connect_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Club_Connect_SwiftUIApp.swift
+//  Club_Connect_SwiftUI
+//
+//  Created by Vishesh Gupta on 06/09/24.
+//
+
+import SwiftUI
+
+@main
+struct Club_Connect_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
