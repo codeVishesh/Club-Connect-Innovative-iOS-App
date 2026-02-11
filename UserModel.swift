f0ead96bc9a9379f1e20c871507fd1d22e245988
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2026-02-11 13:18:33 +0530

    Add files via upload

---
+++ UserModel.swift
@@ -0,0 +1,28 @@
+//
+//  UserModel.swift
+//  Club_Connect
+//
+//  Created by student on 18-04-2024.
+//
+
+import Foundation
+import UIKit
+
+
+struct UserModel{
+    let name: String
+     var categories: [String]!
+    let email: String
+//    let password: String
+//    let college: String
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
