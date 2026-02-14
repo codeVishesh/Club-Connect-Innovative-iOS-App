0e64634957a2ad96858dfa7325791becbed1b847
Vishesh Gupta <vishesh.gupta.jobs@gmail.com>
2026-02-14 22:49:36 +0530

    Add files via upload

--- Club_ConnectTests.swift
+++ Club_ConnectTests.swift
@@ -1,17 +1,36 @@
 //
 //  Club_ConnectTests.swift
-//  Club ConnectTests
+//  Club_ConnectTests
 //
-//  Created by Vishesh Gupta on 21/11/24.
+//  Created by student on 10-04-2024.
 //
 
-import Testing
+import XCTest
 @testable import Club_Connect
 
-struct Club_ConnectTests {
+final class Club_ConnectTests: XCTestCase {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        // Any test you write for XCTest can be annotated as throws and async.
+        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
+        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
     }
 
 }
